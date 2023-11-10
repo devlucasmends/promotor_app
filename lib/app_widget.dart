5df681bace73392ae8ef5de543727fe299b5d7d5
Lucas Mendes <dev.lucasmends@gmail.com>
2023-11-10 07:17:01 -0300

    navigation layer removed

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
-import 'package:promotor_app/src/shared/business/navigation_store.dart';
 import 'package:promotor_app/src/shared/routes/routes.dart';
 
 class AppWidget extends StatelessWidget {
